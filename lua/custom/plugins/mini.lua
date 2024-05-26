8dd6451b306ac98bc997c5f12017b92c315d81ae
Wojtek <wojtek.galaj@hey.com>
2024-05-26 16:52:14 +0200

    use more mini stuff

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -4,6 +4,11 @@ return {
     config = function()
       require("mini.ai").setup()
       require("mini.surround").setup()
+      require("mini.comment").setup()
+      require("mini.ai").setup {}
+      require("mini.surround").setup {}
+      require("mini.comment").setup {}
+      require("mini.statusline").setup {}
     end,
   },
 }
--- lua/custom/plugins/statusline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "echasnovski/mini.statusline",
-    version = false,
-    config = function()
-      require("mini.statusline").setup()
-    end,
-  },
-}
