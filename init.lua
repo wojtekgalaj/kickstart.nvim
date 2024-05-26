07b47aba0c0e314993f2de8f1db81b3614554b0e
Wojtek <wojtek.galaj@hey.com>
2024-05-26 16:35:13 +0200

    chore: code style

--- init.lua
+++ init.lua
@@ -3,18 +3,18 @@
 -- 
 -- Primarily just download and execute lazy.nvim
 --]]
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local uv = vim.loop
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not uv.fs_stat(lazypath) then
   vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
     lazypath,
   }
 end
@@ -24,7 +24,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up lazy, and load my `lua/custom/plugins/` folder
-require('lazy').setup({ import = 'custom/plugins' }, {
+require("lazy").setup({ import = "custom/plugins" }, {
   change_detection = {
     notify = false,
   },
