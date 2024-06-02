e1fc6f0b5df3290e4b8d36be57abc9e96e63cc52
Wojtek <wojtek.galaj@hey.com>
2024-06-02 20:38:21 +0200

    nvim: trouble

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    config = function()
+      require "custom.trouble"
+    end,
+  },
+}
---
+++ lua/custom/trouble.lua
@@ -0,0 +1,7 @@
+local trouble = require "trouble"
+
+trouble.setup {
+  auto_jump = true,
+  focus = true,
+  max_items = 20,
+}
