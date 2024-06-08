f71816b8ab723f03b007c3560fa6be27ed6762d0
Wojtek <wojtek.galaj@hey.com>
2024-06-08 20:06:37 +0200

    lsp stuff but i need to fix it

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -25,8 +25,6 @@ return {
 
       vim.diagnostic.config { virtual_text = false, signs = true, underline = true }
 
-      print(vim.inspect(vim.diagnostic.config())
-
       local capabilities = nil
       if pcall(require, "cmp_nvim_lsp") then
         capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -125,7 +123,6 @@ return {
         "yamlls",
         "cssls",
         "emmet_language_server",
-        "graphql",
       }
 
       vim.list_extend(ensure_installed, servers_to_install)
@@ -175,6 +172,36 @@ return {
         end
       end
 
+      -- Example custom configuration for lua
+      --
+      -- Make runtime files discoverable to the server
+      local runtime_path = vim.split(package.path, ";")
+      table.insert(runtime_path, "lua/?.lua")
+      table.insert(runtime_path, "lua/?/init.lua")
+
+      require("lspconfig").lua_ls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+              version = "LuaJIT",
+              -- Setup your lua path
+              path = runtime_path,
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
+          },
+        },
+      }
       -- Autoformatting Setup
       require("conform").setup {
         formatters_by_ft = {
