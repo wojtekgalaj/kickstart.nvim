afd185fa35a5de4549501b5757bf582924c53959
Wojtek <wojtek.galaj@hey.com>
2024-06-13 11:02:25 +0200

    nvim: remove broken noice config section

--- lua/custom/noice.lua
+++ lua/custom/noice.lua
@@ -12,10 +12,6 @@ require("noice").setup {
   },
   views = {
     cmdline_popup = {
-      border = {
-        style = "none",
-        padding = { 2, 3 },
-      },
       filter_options = {},
       win_options = {
         winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
