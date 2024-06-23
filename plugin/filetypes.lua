77b6cd2e8195020f85d303bdff522ec3a6a2acf9
Wojtek <wojtek.galaj@hey.com>
2024-06-23 09:44:50 +0200

    nvim: setup filtype defs

--- plugin/filetypes.lua
+++ plugin/filetypes.lua
@@ -1,23 +1,6 @@
--- local pattern_type_table = {
---   {
---     pattern = "*.gitlab-ci*.{yml,yaml}",
---     filetype = "yaml.gitlab",
---   },
--- }
---
--- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
---   pattern = "*.gitlab-ci*.{yml,yaml}",
---   callback = function()
---     vim.bo.filetype = "yaml.gitlab"
---   end,
--- })
---
--- function find_filetype_for_pattern(pattern)
---   local p = pattern_type_table
--- end
---
---
-
+--- Holds a collection of filetype patterns and filetypes that should be set for each of them.
+--- @type table<number, {pattern: string, filetype: string}>
+---
 local pattern_type_table = {
   {
     pattern = "*.gitlab-ci*.{yml,yaml}",
@@ -29,13 +12,19 @@ local pattern_type_table = {
   },
 }
 
--- Helper function to check if a file path matches a pattern
+--- Helper function to check if a file path matches a pattern
+--- @param path string The file path to check
+--- @param pattern string The pattern to check against
+---
 local function match_pattern(path, pattern)
   local match = string.match(path, pattern)
   return match ~= nil
 end
 
--- Function to find the filetype for a given file path
+--- Function to find the filetype for a given file path
+--- @param path string The file path to check
+--- @return string|nil The filetype to set
+---
 function find_filetype_for_pattern(path)
   for _, entry in ipairs(pattern_type_table) do
     if match_pattern(path, entry.pattern) then
@@ -45,13 +34,10 @@ function find_filetype_for_pattern(path)
   return nil -- No match found
 end
 
--- Set the filetype based on the pattern
+-- Set the filetype based on a regular expression match on the file path
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
-    local path = vim.fn.expand "%:p" -- Get the full file path
-    if path then
-      print("buf read path" .. path)
-    end
+    local path = vim.fn.expand "%:p"
     local filetype = find_filetype_for_pattern(path)
     if filetype then
       print("buf read filetype" .. filetype)
